aaff07f8246fda3224b923b5f0149f5bfd996aa6
Job <job@scrumble.nl>
2020-07-14 13:04:01 +0200

    Remove defaulting to sm from size prop

--- src/modal.tsx
+++ src/modal.tsx
@@ -66,7 +66,7 @@ class Modal extends React.Component<props & ModalProps, state> {
 
     render = (): JSX.Element => {
         return (
-            <BootstrapModal show={this.state.show} onHide={this.handleClose.bind(this)} size={this.props.size || 'sm'}>
+            <BootstrapModal show={this.state.show} onHide={this.handleClose.bind(this)} size={this.props.size}>
                 <BootstrapModal.Header closeButton>
                     <BootstrapModal.Title>{this.props.title}</BootstrapModal.Title>
                 </BootstrapModal.Header>
